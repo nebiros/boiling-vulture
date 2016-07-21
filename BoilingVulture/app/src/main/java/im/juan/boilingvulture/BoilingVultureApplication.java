542dcd9b91e568dd9985befd9d52f6bafbaa87d5
Juan Felipe Alvarez Saldarriaga <nebiros@gmail.com>
2016-07-21 18:08:00 -0500

    code format and some dependencies added

---
+++ BoilingVulture/app/src/main/java/im/juan/boilingvulture/BoilingVultureApplication.java
@@ -0,0 +1,10 @@
+package im.juan.boilingvulture;
+
+import android.app.Application;
+
+public class BoilingVultureApplication extends Application {
+
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
--- BoilingVulture/app/src/main/java/im/juan/boilingvulture/ui/main/MainActivityFragment.java
+++ BoilingVulture/app/src/main/java/im/juan/boilingvulture/ui/main/MainActivityFragment.java
@@ -1,10 +1,12 @@
-package im.juan.boilingvulture;
+package im.juan.boilingvulture.ui.main;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import im.juan.boilingvulture.R;
 
 /**
  * A placeholder fragment containing a simple view.
